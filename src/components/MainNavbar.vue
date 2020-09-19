3e3607d7dd5e9418e0d77e258b5d69da3480c37d
Martin Blascak <martin.blascak86@gmail.com>
2020-09-19 08:51:53 +0200

    responsive menu completed

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -6,7 +6,16 @@
       <router-link to="/contact">Contact</router-link>
       <router-link to="/login">Login</router-link>
     </div>
-    <menuBtn class="menuBtn" />
+    <div class="navRouterWrapVertical" :class="{ navRouterWrapVerticalSlide : showRespMenu}">
+      <div class="verticalLogoBg"></div>
+      <router-link to="/">About</router-link> 
+      <router-link to="/work">Work</router-link> 
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <div class="menuBtnWrap" @click="showRespMenu = !showRespMenu">
+      <menuBtn class="menuBtn"  />  
+    </div>
   </div>
 </template>
 
@@ -20,7 +29,7 @@ export default {
   },
   data() {
     return {
-
+      showRespMenu: true
     }
   }
 }
@@ -30,6 +39,7 @@ export default {
 <style scoped>
 
 .mainNavbar {
+  position: relative;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -37,11 +47,11 @@ export default {
   padding: 16px 16px 16px 30px;
   background: #1f1f1f;
   border-bottom: 1px solid #20b01e;
-  box-shadow: 2px 2px 2px black;
+  /* box-shadow: 2px 2px 2px black; */
   z-index: 10;
 }
 
-.mainNavbar a {
+.mainNavbar .navRouterWrap a {
   display: inline-block;
   font-weight: 900;
   text-decoration: none;
@@ -55,7 +65,7 @@ export default {
   -webkit-box-reflect: below 1px linear-gradient(transparent, #0005);
 }
 
-.mainNavbar a.router-link-exact-active {
+.mainNavbar .navRouterWrap a.router-link-exact-active {
   background-color: #20b01e;
   color: #3a3a3a;
   box-shadow: 0 0 5px #20b01e,
@@ -64,6 +74,43 @@ export default {
               0 0 200px #20b01e;
 }
 
+.navRouterWrapVertical {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 250px;
+  background: rgba(31, 31, 31, 0.734);
+  border-right: 1px solid #20b01e;
+  /* padding-top: 4rem; */
+  transition: all 0.5s ease-in-out;
+}
+
+.navRouterWrapVertical a {
+  display: block;
+  font-weight: 900;
+  text-decoration: none;
+  color: #20b01e;
+  transition: 0.3s;
+  letter-spacing: 3px;
+  font-size: 0.9rem;
+  margin: 2rem 0.5rem 0 0.5rem;
+  padding: 0.5rem 0.75rem;
+  text-transform: uppercase;
+}
+
+.navRouterWrapVertical a.router-link-exact-active {
+  background-color: #20b01e;
+  color: #3a3a3a;
+}
+
+.verticalLogoBg {
+  width: 100%;
+  height: 5.2rem;
+  background: #1f1f1f;
+}
+
+.navRouterWrapVertical,
 .menuBtn {
   display: none;
 }
@@ -74,9 +121,20 @@ export default {
 
 @media (max-width: 650px) {
 
+    .navRouterWrapVertical,
     .menuBtn {
       display: inline-block;
     }
+
+    .navRouterWrapVerticalSlide {
+      /* width: 0; */
+      transform: translateX(-300px);
+    }
+
+    /* .navRouterWrapVerticalSlide a {
+      display: none;
+    } */
+
     .navRouterWrap {
       display: none;
     }
